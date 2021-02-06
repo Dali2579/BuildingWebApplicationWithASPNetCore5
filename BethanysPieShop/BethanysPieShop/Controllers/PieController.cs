5a01c18a7dc3ac55b749b08f917b7b672849f388
dali2579 <ma.benamor@gmail.com>
2021-02-06 11:16:20 +0100

    Add a PieController that will return the list of pies using the PieMockRepository

---
+++ BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,25 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController:Controller
+    {
+        private readonly IPieRepository pieRepository;
+        private readonly ICategoryRepository categoryRepository;
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this.pieRepository = pieRepository;
+            this.categoryRepository = categoryRepository;
+        }
+        public ViewResult List()
+        {
+            return View(this.pieRepository.AllPies);
+        }
+
+    }
+}
